6accc2a31136eca6268424e0be488587d79dbf9a
swajan-75 <swajanbarua09@gmail.com>
2026-03-18 18:00:34 +0600

    smtp fixed

--- internal/service/otp_service.go
+++ internal/service/otp_service.go
@@ -6,13 +6,16 @@ import (
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"log"
+	"strconv"
 	"time"
 
-	"net/smtp"
+	//"net/smtp"
 	"os"
 	"portfolio_backend_go/internal/repository"
 
 	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/gomail.v2"
 )
 
 type Otp_service struct {
@@ -28,27 +31,59 @@ func (s *Otp_service) Send_otp(c context.Context, email string, IdToken string) 
 	rand.Read(otp)
 	code := fmt.Sprintf("%06d", int(otp[0])%10*100000+int(otp[1])%10*10000+int(otp[2])%10*1000)
 
+	
+
+
+
+
+
 	FromEmail := os.Getenv("SMTP_EMAIL")
 	Password := os.Getenv("SMTP_PASSWORD")
 	smtpHost := os.Getenv("SMTP_HOST")
 	smtpPort := os.Getenv("SMTP_PORT")
-	address := smtpHost + ":" + smtpPort
+	smtp_Port_int,err := strconv.Atoi(smtpPort);
+	if err != nil {
+		smtp_Port_int = 587;
+	}
+	//address := smtpHost + ":" + smtpPort
+
+	m := gomail.NewMessage()
+	m.SetHeader("From",FromEmail)
+	m.SetHeader("To",email)
+	m.SetHeader("Subject", "Your Portfolio Admin OTP")
+    m.SetBody("text/html", "Your OTP code is: <b>" + code + "</b>")
+
+	d := gomail.NewDialer(
+        smtpHost, 
+        smtp_Port_int, 
+        FromEmail, 
+        Password,
+    )
+
+    // This is the part that prevents the "Hang"
+    if err := d.DialAndSend(m); err != nil {
+        log.Printf("❌ SMTP Error: %v", err)
+        return err
+    }
+    return nil
+
+
 	//fmt.Printf("DEBUG: Host is |%s|\n", smtpHost)
 	// fmt.Printf("DEBUG: Port is |%s|\n", smtpPort)
 
-	auth := smtp.PlainAuth(
-		"",
-		FromEmail,
-		Password,
-		smtpHost,
-	)
-	msg := []byte("Subject: Portfolio Login OTP\r\n\r\nYour code is: " + code)
+	// auth := smtp.PlainAuth(
+	// 	"",
+	// 	FromEmail,
+	// 	Password,
+	// 	smtpHost,
+	// )
+	// msg := []byte("Subject: Portfolio Login OTP\r\n\r\nYour code is: " + code)
 
-	err := smtp.SendMail(address, auth, FromEmail, []string{email}, msg)
-	if err != nil {
-		return errors.New("unable to send OTP email. please check your network or configuration")
+	// err := smtp.SendMail(address, auth, FromEmail, []string{email}, msg)
+	// if err != nil {
+	// 	return errors.New("unable to send OTP email. please check your network or configuration")
 
-	}
+	// }
 	//safeEmail := base64.StdEncoding.EncodeToString([]byte(email))
 
 	safeEmail := safe_email(email)
