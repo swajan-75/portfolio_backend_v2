81b5f70485e7ba796a89c102468445a1fd96209c
swajan-75 <swajanbarua09@gmail.com>
2026-03-19 18:46:59 +0600

    update

--- internal/api/handlers/Admin_handler.go
+++ internal/api/handlers/Admin_handler.go
@@ -41,8 +41,9 @@ func (h *Admin_handler) Admin_Login(c * gin.Context){
 
 }
 func (h *Admin_handler) Logout(c *gin.Context) {
-    c.SetCookie("access_token", "", -1, "/", "", true, true)
 	c.SetSameSite(http.SameSiteNoneMode)
+    c.SetCookie("access_token", "", -1, "/", "", true, true)
+	
 
     c.JSON(http.StatusOK, gin.H{
         "message": "Logged out successfully",
